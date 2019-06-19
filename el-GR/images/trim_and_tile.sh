57accec20b7e55fd476abc9ba9927ebd6c75483d
majamanojlovic <49232422+majamanojlovic@users.noreply.github.com>
2019-06-19 14:57:43 +0100

    New translations trim_and_tile.sh (Greek)

--- el-GR/images/trim_and_tile.sh
+++ el-GR/images/trim_and_tile.sh
@@ -1,7 +1,7 @@
 for file in 1*.png;
 do
-  text=$(echo "$file" | head -c 2 | tail -c 1) #get 2nd character from file name
-  convert $file -trim +repage $file; #trim the image
-  convert $file -gravity South -splice 0x36 -font FreeSans -pointsize 36 -annotate +0+-5 "$text" label$file; #annotate with text
+  text=$(echo "$file" | head -c 2 | tail -c 1) #πάρε τον δεύτερο χαρακτήρα από το όνομα του αρχείου
+  convert $file -trim +repage $file; #επεξεργάσου την εικόνα
+  convert $file -gravity South -splice 0x36 -font FreeSans -pointsize 36 -annotate +0+-5 "$text" label$file; #πρόσθεσε κείμενο
 done
-montage label*.png -tile 2x2 -geometry +5+1  montage.png #montage
+montage label*.png -tile 2x2 -geometry +5+1  montage.png #μοντάζ
