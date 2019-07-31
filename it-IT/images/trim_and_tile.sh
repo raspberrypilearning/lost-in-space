eb14a1ab8caf0d85b44377a955bf035e2aebf308
majamanojlovic <49232422+majamanojlovic@users.noreply.github.com>
2019-07-31 12:25:03 +0100

    New translations trim_and_tile.sh (Italian)

--- it-IT/images/trim_and_tile.sh
+++ it-IT/images/trim_and_tile.sh
@@ -1,6 +1,6 @@
 for file in 1*.png;
 do
-  text=$(echo "$file" | head -c 2 | tail -c 1) #get 2nd character from file name
+  text = $ (echo "$file" | head -c 2 | tail -c 1) #get 2a carattere dal nome del file
   convert $file -trim +repage $file; #trim the image
   convert $file -gravity South -splice 0x36 -font FreeSans -pointsize 36 -annotate +0+-5 "$text" label$file; #annotate with text
 done
