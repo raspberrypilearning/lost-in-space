5aa818498d95398a475c5775cf2dd779cb0d692c
majamanojlovic <49232422+majamanojlovic@users.noreply.github.com>
2019-12-03 11:27:51 +0000

    New translations trim_and_tile.sh (Croatian)

--- hr-HR/images/trim_and_tile.sh
+++ hr-HR/images/trim_and_tile.sh
@@ -1,7 +1,7 @@
 for file in 1*.png;
 do
-  text=$(echo "$file" | head -c 2 | tail -c 1) #get 2nd character from file name
-  convert $file -trim +repage $file; #trim the image
-  convert $file -gravity South -splice 0x36 -font FreeSans -pointsize 36 -annotate +0+-5 "$text" label$file; #annotate with text
+  text=$(echo "$file" | head -c 2 | tail -c 1) #dohvati drugi znak iz imena
+  convert $file -trim +repage $file; #trimaj sliku
+  convert $file -gravity South -splice 0x36 -font FreeSans -pointsize 36 -annotate +0+-5 "$text" label$file; #napomena uz tekst
 done
-montage label*.png -tile 2x2 -geometry +5+1  montage.png #montage
+montage label*.png -tile 2x2 -geometry +5+1  montage.png #montaža
