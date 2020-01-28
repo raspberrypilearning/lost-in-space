fdd4bf0b2ca1da331f1814f9c639d09392ed72b3
majamanojlovic <49232422+majamanojlovic@users.noreply.github.com>
2020-01-28 13:44:40 +0000

    New translations trim_and_tile.sh (Sinhala)

--- si-LK/images/trim_and_tile.sh
+++ si-LK/images/trim_and_tile.sh
@@ -4,4 +4,4 @@ do
   convert $file -trim +repage $file; #trim the image
   convert $file -gravity South -splice 0x36 -font FreeSans -pointsize 36 -annotate +0+-5 "$text" label$file; #annotate with text
 done
-montage label*.png -tile 2x2 -geometry +5+1 montage.png #montage
+montage label*.png -tile 2x2 -geometry +5+1  montage.png #montage
